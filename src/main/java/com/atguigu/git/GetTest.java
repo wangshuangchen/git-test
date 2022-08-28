242c7674dcc562293ce8300ff3f65e1c20ee37d8
root <root@atguigu.com>
2022-08-28 14:34:00 +0800

    second commit

--- src/main/java/com/atguigu/git/GetTest.java
+++ src/main/java/com/atguigu/git/GetTest.java
@@ -3,6 +3,7 @@ package com.atguigu.git;
 public class GetTest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world2");
     }
 
 }
