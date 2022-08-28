774f29cf9bc7f052f8b5001effc7c4eb8309e2b8
root <root@atguigu.com>
2022-08-28 14:48:35 +0800

    fanmaster commit2

--- src/main/java/com/atguigu/git/GetTest.java
+++ src/main/java/com/atguigu/git/GetTest.java
@@ -6,6 +6,7 @@ public class GetTest {
         System.out.println("hello world2");
         System.out.println("hello world3");
         System.out.println("hello world4 -hot-fix");
+        System.out.println("master 和刚才的master分支写反了");
     }
 
 }
