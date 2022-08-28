a642d16875640cd13ae528a2c0a9b6d2a607bc74
root <root@atguigu.com>
2022-08-28 14:43:36 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GetTest.java
+++ src/main/java/com/atguigu/git/GetTest.java
@@ -5,6 +5,7 @@ public class GetTest {
         System.out.println("hello world");
         System.out.println("hello world2");
         System.out.println("hello world3");
+        System.out.println("hello world4 -hot-fix");
     }
 
 }
